5132f763da128770bd1a7b815a588634dddf6efa
Kevin <kn880701@gmail.com>
2024-07-10 10:52:28 +0800

    [feat]更新地圖資訊、修改主頁面tab未點擊時顏色

--- app/src/main/java/com/lhr/water/ui/base/AppViewModelFactory.kt
+++ app/src/main/java/com/lhr/water/ui/base/AppViewModelFactory.kt
@@ -30,7 +30,7 @@ class AppViewModelFactory(
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when (modelClass) {
             CoverViewModel::class.java -> CoverViewModel(context, formRepository, regionRepository, userRepository) as T
-            LoginViewModel::class.java -> LoginViewModel(context, regionRepository) as T
+            LoginViewModel::class.java -> LoginViewModel(context, formRepository, regionRepository, userRepository) as T
             MainViewModel::class.java -> MainViewModel(context, formRepository) as T
             FormContentViewModel::class.java -> FormContentViewModel(context, formRepository) as T
             FormViewModel::class.java -> FormViewModel(context, regionRepository, formRepository) as T
