4d990af31620e5cd5f8818ff7a747708c43257dc
Kevin <kn880701@gmail.com>
2024-07-10 11:36:03 +0800

    [ver]1.0.3+4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.lhr.water"
         minSdk = 26
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
